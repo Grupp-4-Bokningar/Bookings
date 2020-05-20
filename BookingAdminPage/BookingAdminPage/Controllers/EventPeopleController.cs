26784cd9f164abe9c286dba2cc918a36bd94e4b5
brunzt <61004397+brunzt@users.noreply.github.com>
2020-05-20 10:50:50 +0200

    testing bibo
    walla

---
+++ BookingAdminPage/BookingAdminPage/Controllers/EventPeopleController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Newtonsoft.Json;
+
+namespace BookingAdminPage.Controllers
+{
+    public class EventPeopleController : Controller
+    {
+        // GET: EventPeople
+        string baseUrl = "193.10.202.81/BookingService";
+        // GET: Rest
+        public async Task<ActionResult> Index()
+        {
+            try
+            {
+                List<IllegalaDjur> IllegalaDjurInfo = new List<IllegalaDjur>();
+
+                using (var client = new HttpClient())
+                {
+                    //Passing service base url  
+                    client.BaseAddress = new Uri(baseUrl);
+
+                    client.DefaultRequestHeaders.Clear();
+                    //Define request data format  
+                    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                    //Sending request to find web api REST service resource GetAllEmployees using HttpClient  
+                    HttpResponseMessage Res = await client.GetAsync("IllegalaDjur");
+
+                    //Checking the response is successful or not which is sent using HttpClient  
+                    if (Res.IsSuccessStatusCode)
+                    {
+                        //Storing the response details recieved from web api   
+                        var IllegalaDjurResponse = Res.Content.ReadAsStringAsync().Result;
+
+                        //Deserializing the response recieved from web api and storing into the Employee list  
+                        IllegalaDjurInfo = JsonConvert.DeserializeObject<List<IllegalaDjur>>(IllegalaDjurResponse);
+
+                    }
+                    //returning the employee list to view  
+                    return View(IllegalaDjurInfo);
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Hoppsan! här blev det fel.");
+                throw;
+            }
+
+        }
+    }
+}
