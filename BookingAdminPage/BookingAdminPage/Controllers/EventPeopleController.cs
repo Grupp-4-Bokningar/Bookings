e40c7bd5ebf35d091eb98a8fe35a782a56783850
b3ngtsson <43953348+b3ngtsson@users.noreply.github.com>
2020-05-22 12:15:02 +0200

    Merged files, now working togheter

--- BookingAdminPage/BookingAdminPage/Controllers/EventPeopleController.cs
+++ BookingAdminPage/BookingAdminPage/Controllers/EventPeopleController.cs
@@ -53,5 +53,44 @@ namespace BookingAdminPage.Controllers
             }
 
         }
+        public async Task<ActionResult> SpecificEvent(int id)
+        {
+            try
+            {
+                List<AdminDataModell> EventInfo = new List<AdminDataModell>();
+
+                using (var client = new HttpClient())
+                {
+                    //Passing service base url  
+                    client.BaseAddress = new Uri(baseUrl);
+
+                    client.DefaultRequestHeaders.Clear();
+                    //Define request data format  
+                    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                    //Sending request to find web api REST service resource GetAllEmployees using HttpClient  
+                    HttpResponseMessage Res = await client.GetAsync("api/Bookings/Event/" +id +"/Visitor");
+
+                    //Checking the response is successful or not which is sent using HttpClient  
+                    if (Res.IsSuccessStatusCode)
+                    {
+                        //Storing the response details recieved from web api   
+                        var AdminDataModellResponse = Res.Content.ReadAsStringAsync().Result;
+
+                        //Deserializing the response recieved from web api and storing into the Employee list  
+                        EventInfo = JsonConvert.DeserializeObject<List<AdminDataModell>>(AdminDataModellResponse);
+
+                    }
+                    //returning the employee list to view  
+                    return View(EventInfo);
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Hoppsan! här blev det fel.");
+                throw;
+            }
+
+        }
     }
 }
