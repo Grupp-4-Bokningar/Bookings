46189c82a8b4eac05b4a8e2bfbf06f603c65cc50
hugolinlycke <61004847+hugolinlycke@users.noreply.github.com>
2020-05-25 10:48:23 +0200

    New Stable Verision
    ID SSKICKAS LOL

--- BookingService/BookingService/Models/BookingModell.cs
+++ BookingService/BookingService/Models/BookingModell.cs
@@ -8,9 +8,11 @@ namespace BookingService.Models
     public class BookingModell
     {
         public int Booking_Id { get; set; }
+        public int Event_Id { get; set; }
         public string EventNamn { get; set; }
         public DateTime Startdate { get; set; }
         public DateTime Enddate { get; set; }
+        public int User_Id { get; set; }
         public string User_Name { get; set; }
         public string User_Type { get; set; }
     }
