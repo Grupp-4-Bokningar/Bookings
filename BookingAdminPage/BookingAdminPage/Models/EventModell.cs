96066c72c1e5fa9ff825eaa24b7de2bcd2b9716c
Mauro Karlsfred <mauro@karlsfred.se>
2020-05-27 11:18:05 +0200

    New model
    New model for events (Category)

--- BookingAdminPage/BookingAdminPage/Models/EventModell.cs
+++ BookingAdminPage/BookingAdminPage/Models/EventModell.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,17 +9,31 @@ namespace BookingAdminPage.Models
     public class EventModell
     {
         public int Event_Id { get; set; }
+
+        [StringLength(50)]
+
         public string Event_Name { get; set; }
+
         public bool Event_Active { get; set; }
+
         public int Event_Arranger_Id { get; set; }
-        public bool Event_Seeking_Volunteers { get; set; }
-        public int Event_Location_Id { get; set; }
-        public string Event_Category_Name { get; set; }
+
+        public bool? Event_Seeking_Volunteers { get; set; }
+
+        public int? Event_Location_Id { get; set; }
+
+        public Category Event_Category { get; set; }
+
         public string Event_Description { get; set; }
+
         public string Event_Imagelink { get; set; }
-        public int Event_Ticket_Price { get; set; }
-        public DateTime Event_Start_Datetime { get; set; }
-        public DateTime Event_End_Datetime { get; set; }
-        public DateTime Event_Create_Datetime { get; set; } 
+
+        public int? Event_Ticket_Price { get; set; }
+
+        public DateTime? Event_Start_Datetime { get; set; }
+
+        public DateTime? Event_End_Datetime { get; set; }
+
+        public DateTime? Event_Create_Datetime { get; set; }
     }
 }
