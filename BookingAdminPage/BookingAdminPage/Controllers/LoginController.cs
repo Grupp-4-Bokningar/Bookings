c42e2f660dd30c54712389d35f519e250bb7a9d6
brunzt <61004397+brunzt@users.noreply.github.com>
2020-05-28 09:49:50 +0200

    fucklord
    ass

--- BookingAdminPage/BookingAdminPage/Controllers/LoginController.cs
+++ BookingAdminPage/BookingAdminPage/Controllers/LoginController.cs
@@ -6,12 +6,40 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Security;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
+using System.Net;
 
 namespace BookingAdminPage.Controllers
 {
     public class LoginController : Controller
     {
+        string baseUrl = "http://193.10.202.76/";
         // GET: Login
+        [HttpPost]
+        public async Task<bool> CheckLogin([Bind(Include = "Id,username,password,permission")] CredentialsRecived creds)
+        {
+            if (ModelState.IsValid)
+            {
+                using (var client = new HttpClient())
+                {
+                    client.BaseAddress = new Uri(baseUrl);
+
+                    var response = await client.PostAsJsonAsync("api/bookinglogin", creds);
+                    if (response.IsSuccessStatusCode)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        ModelState.AddModelError(string.Empty, "Server error try after some time.");
+                    }
+                }
+            }
+            return false;
+        }
         public ActionResult Login()
         {
             return View();
@@ -22,7 +50,6 @@ namespace BookingAdminPage.Controllers
             //if else om loginnen lyckas eller ej
             if (IsValid(model))
             {
-                //ändra false till true om vi vill ha en permanent cookie
                 FormsAuthentication.SetAuthCookie(model.username, false);
                 return Redirect(ReturnUrl);
             }
@@ -33,7 +60,14 @@ namespace BookingAdminPage.Controllers
         }
         private bool IsValid(Credentials model)
         {
-            return (model.username == "test" && model.password == "test");
+            //ändra false till true om vi vill ha en permanent cookie
+            //ska bara posta stupid
+            CredentialsRecived cr = new CredentialsRecived();
+            cr.Id = 0;
+            cr.username = model.username;
+            cr.password = model.password;
+            cr.password = "bookingadmin";
+            return CheckLogin(cr)/*(model.username == "test" && model.password == "test")*/;
         }
     }
 }
