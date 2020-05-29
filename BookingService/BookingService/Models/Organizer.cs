27f05e64bdc0a59a905c4994b68eb13f2ce33615
hugolinlycke <61004847+hugolinlycke@users.noreply.github.com>
2020-05-29 09:38:58 +0200

    Model Change

---
+++ BookingService/BookingService/Models/Organizer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace Organizer.Models
+{
+    public class Organizer
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Name { get; set; }
+        public string Phone { get; set; }
+    }
+}
