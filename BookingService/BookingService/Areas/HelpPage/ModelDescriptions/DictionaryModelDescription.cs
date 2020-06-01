ecdd323ed70b25d709ce0cefa8ae6f72df4fbccd
Mauro Karlsfred <mauro@karlsfred.se>
2020-06-01 10:50:59 +0200

    Deleted BookingService

--- BookingService/BookingService/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace BookingService.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- BookingService/BookingService/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace BookingService.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
